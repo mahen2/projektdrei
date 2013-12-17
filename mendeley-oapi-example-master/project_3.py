d9ac1b83e17dc8902623242aedb8e48c15fb8d0c
Dennis Weber <D.Weber@hhu.de>
2013-12-17 19:22:09 +0100

    Added comments

--- mendeley-oapi-example-master/project_3.py
+++ mendeley-oapi-example-master/project_3.py
@@ -17,6 +17,7 @@ def open_from_pickle(filename):
         r_object = pickle.load(p_input)
     return r_object
     
+# Draws a barchart with the matplotlib-library using the given values. 
 def draw_barchart(names, values, ylabel, title):
     ind=range(len(values))
     fig = p.figure()
@@ -29,9 +30,11 @@ def draw_barchart(names, values, ylabel, title):
     fig.autofmt_xdate()
     p.show()    
 
+# Draws a piechart with the matplotlib-library using the given values. 
 def draw_piechart(names, values):
     t= 0
     sizes=[]
+    # Convert the given values to percentages/100
     for element in values:
         t = t+element
     for element in values:
@@ -41,6 +44,7 @@ def draw_piechart(names, values):
     p.axis('equal')
     p.show()
 
+# Draws a timeline with the matplotlib-library using the given values and dates. 
 def draw_timeline(names, values, ylabel, title):
     p.plot_date(x=names, y=values, fmt="-")
     p.ylabel(ylabel)
@@ -99,30 +103,34 @@ if __name__ == "__main__":
 
     # ### Plotting ###
 
+    # Reads the file with the extracted tags and makes two ordered lists (tags and frequency) out of it. Then visualizes the results with a barchart.
     """
     # Top20 Tags
     tags = open_from_pickle("top20tags")
     tags_value = []
     tags_name = []
+    # Split the tag/frequency pairs in two lists
     for element in tags:
         tags_value.append(element["count"])
         tags_name.append(element["name"].encode("utf-8"))
     draw_barchart(tags_name, tags_value, "Haeufigkeit des vergebenen Tags", "Top20 Tags der Kategorie 'Computer and Informationscience'")
     """
     
-    
+    # Draws a piechart giving the percentage distribution of the overall publications of the last 10 years.
     """
     # Overall Publications
     publications = open_from_pickle("overall_pub")
     pub_years = {}
     pub_syears = []
     pub_scount = []
+    # Check if the publication year is in the last 10 years. If yes, add it to the dictionary or increase its frequency
     for element in publications:
         if element["year"] >=2003:
             if element["year"] in pub_years:
                 pub_years[element["year"]] += 1
             else:
                 pub_years[element["year"]] = 1
+    # Make a list of tuples sorted by the frequency ot of the dictionary
     sorted_years = sorted(pub_years.iteritems(), key=operator.itemgetter(0))
     for element in sorted_years:
         pub_syears.append(element[0])
@@ -130,6 +138,7 @@ if __name__ == "__main__":
     draw_piechart(pub_syears, pub_scount)
     """
     
+    # Draws a timeline for the publications of "Wolfgang G. Stock".
     """
     # Stock Publications/Year
     publications = open_from_pickle("pub_stock")
@@ -138,11 +147,14 @@ if __name__ == "__main__":
     all_years = []
     pub_syears = []
     pub_scount = []
+    # Get all of the years, an article was published and sort them.
     for element in publications["documents"]:
         pub_years.append(element["year"])
     pub_years=sorted(pub_years)
+    # Make a List with all years in his publication-time.
     for i in range(pub_years[0], pub_years[-1]):
         all_years.append(i)
+    # Check if he published something in a year and cycle the publications. If yes, add it to the dictionary or increase the frequency, else add with 0 frequency to the dictionary
     for year in all_years:
         if year in pub_years:
             for element in publications["documents"]:
@@ -153,19 +165,24 @@ if __name__ == "__main__":
                         pub_dic[element["year"]] = 1
         else:
             pub_dic[year] = 0
+    # Sort by year, not by frequency this time
     sorted_years = sorted(pub_dic.iteritems(), key=operator.itemgetter(0))
     for element in sorted_years:
+        # Convert the integer years to the datetime-format, so we can use the dateplot-function of matplotlib
         pub_syears.append(datetime.strptime(str(element[0]) , '%Y'))
         pub_scount.append(element[1])
     draw_timeline(pub_syears, pub_scount, "Anzahl der Publikationen", "Anzahl der Publikationen/Jahr von Wolfgang G. Stock")
     """
     
+    # Draws a barchart of all of the authors, who co-authored Wolfgang G. Stock and sorts them by the number of co-published papers
     """
     # Stock Co-Authors
+    # Save the data for all publications of Stock in a list
     publications = open_from_pickle("pub_stock")["documents"]
     authors = {}
     authors_value = []
     authors_name = []
+    # For each article: get all of the names of the authors and if it is not "Wolfgang G. Stock" then save them with their frequency of co-authorhood in a dictionary.
     for article in publications:
         for author in article["authors"]:
             if (author["surname"] != "Stock" and author["forename"] != "Wolfgang G.") and (author["surname"] != "G. Stock" and author["forename"] != "Wolfgang"):
@@ -181,6 +198,7 @@ if __name__ == "__main__":
     draw_barchart(authors_name, authors_value, "Anzahl gem. publizierter Artikel", "Co-Autoren von Wolfgang G. Stock") 
     """
     
+    # Draws a piechart given the frequencys a category on mendeley was tagged with "ontology".
     """
     # Ontology
     ontology = open_from_pickle("onto_tagged")
