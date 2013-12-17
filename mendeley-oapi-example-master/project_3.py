6f9c3f9cddf5f861d6297cdcb129b72ba5a0c198
Maurice S <Maurice.Schleussinger@uni-duesseldorf.de>
2013-12-17 23:37:38 +0100

    uncomment all the code for final version

--- mendeley-oapi-example-master/project_3.py
+++ mendeley-oapi-example-master/project_3.py
@@ -59,57 +59,57 @@ if __name__ == "__main__":
     # ### Collect the required data ###
 
     mendeley = create_client()
-    # categories = mendeley.categories()
+    categories = mendeley.categories()
 
     # Export all categories for later usage
-    # with open("categories.json", "wb") as json_output:
-    #     json.dump(categories, json_output, indent=4)
+    with open("categories.json", "wb") as json_output:
+        json.dump(categories, json_output, indent=4)
 
     # Overall publications publications of all time
-    # overall_pub = {}
-    # for i in range(2003, 2013):
-    #     overall_pub[i]=mendeley.search("year:%s"%i)["total_results"]
-    # save_as_pickle(overall_pub, "overall_pub")
+    overall_pub = {}
+    for i in range(2003, 2013):
+        overall_pub[i]=mendeley.search("year:%s"%i)["total_results"]
+    save_as_pickle(overall_pub, "overall_pub")
 
     # # Top 20 Tags from Computer and Information Science (cat 6)
-    # top20tags = mendeley.tag_stats(6)
-    # save_as_pickle(top20tags, "top20tags")
+    top20tags = mendeley.tag_stats(6)
+    save_as_pickle(top20tags, "top20tags")
 
     # All publications by the author Wolfgang G Stock
-    # pub_stock = mendeley.search("author:\"Wolfgang G Stock\"", items=500)
-    # save_as_pickle(pub_stock, "pub_stock")
+    pub_stock = mendeley.search("author:\"Wolfgang G Stock\"", items=500)
+    save_as_pickle(pub_stock, "pub_stock")
 
     # Top 10 publications published in "Nature"
-    # search_nature = mendeley.search("published_in:\"Nature\"", items=10)
-    # top10_nature = {}
-    # for elem in search_nature["documents"]:
-    #     top10_nature[elem["title"]] = mendeley.details(elem["uuid"])\
-    #                                    ["stats"]["readers"]
-    # save_as_pickle(top10_nature, "top10_nature")
+    search_nature = mendeley.search("published_in:\"Nature\"", items=10)
+    top10_nature = {}
+    for elem in search_nature["documents"]:
+        top10_nature[elem["title"]] = mendeley.details(elem["uuid"])\
+                                       ["stats"]["readers"]
+    save_as_pickle(top10_nature, "top10_nature")
 
     # All Publications with the tag "ontology"
-    # onto_tagged = {}
-    # for category in categories:
-    #     cat_id = category["id"]
-    #     onto_tagged[cat_id] = 0
-    #     # If the item value is too high (not max 500 as with .search)
-    #     # no documents are returned
-    #     # (maybe the item value must be less than the number of results)
-    #     page_count = mendeley.tagged("ontology", cat=cat_id, items=100)\
-    #                  ["total_pages"]
-    #     for page_num in range(1, page_count+1):
-    #         tagged = mendeley.tagged("ontology", cat=cat_id, items=100,
-    #                                  page=page_num)
-    #         for document in tagged["documents"]:
-    #             if document["year"]==2011:
-    #                 onto_tagged[cat_id] += 1
-    # save_as_pickle(onto_tagged, "onto_tagged")
+    onto_tagged = {}
+    for category in categories:
+        cat_id = category["id"]
+        onto_tagged[cat_id] = 0
+        # If the item value is too high (not max 500 as with .search)
+        # no documents are returned
+        # (maybe the item value must be less than the number of results)
+        page_count = mendeley.tagged("ontology", cat=cat_id, items=100)\
+                     ["total_pages"]
+        for page_num in range(1, page_count+1):
+            tagged = mendeley.tagged("ontology", cat=cat_id, items=100,
+                                     page=page_num)
+            for document in tagged["documents"]:
+                if document["year"]==2011:
+                    onto_tagged[cat_id] += 1
+    save_as_pickle(onto_tagged, "onto_tagged")
 
 
     # ### Plotting ###
 
     # Reads the file with the extracted tags and makes two ordered lists (tags and frequency) out of it. Then visualizes the results with a barchart.
-    """
+
     # Top20 Tags
     tags = open_from_pickle("top20tags")
     tags_value = []
@@ -119,32 +119,32 @@ if __name__ == "__main__":
         tags_value.append(element["count"])
         tags_name.append(element["name"].encode("utf-8"))
     draw_barchart(tags_name, tags_value, "Haeufigkeit des vergebenen Tags", "Top20 Tags der Kategorie 'Computer and Informationscience'")
-    """
+
 
     # Draws a piechart giving the percentage distribution of the overall publications of the last 10 years.
 
     # Overall Publications
-    # publications = open_from_pickle("overall_pub")
-    # pub_years = {}
-    # pub_syears = []
-    # pub_scount = []
-    # # Check if the publication year is in the last 10 years. If yes, add it to the dictionary or increase its frequency
-    # for element in publications:
-    #     if element["year"] >=2003:
-    #         if element["year"] in pub_years:
-    #             pub_years[element["year"]] += 1
-    #         else:
-    #             pub_years[element["year"]] = 1
-    # # Make a list of tuples sorted by the frequency ot of the dictionary
-    # sorted_years = sorted(pub_years.iteritems(), key=operator.itemgetter(0))
-    # for element in sorted_years:
-    #     pub_syears.append(element[0])
-    #     pub_scount.append(element[1])
-    # draw_piechart(pub_syears, pub_scount)
+    publications = open_from_pickle("overall_pub")
+    pub_years = {}
+    pub_syears = []
+    pub_scount = []
+    # Check if the publication year is in the last 10 years. If yes, add it to the dictionary or increase its frequency
+    for element in publications:
+        if element["year"] >=2003:
+            if element["year"] in pub_years:
+                pub_years[element["year"]] += 1
+            else:
+                pub_years[element["year"]] = 1
+    # Make a list of tuples sorted by the frequency ot of the dictionary
+    sorted_years = sorted(pub_years.iteritems(), key=operator.itemgetter(0))
+    for element in sorted_years:
+        pub_syears.append(element[0])
+        pub_scount.append(element[1])
+    draw_piechart(pub_syears, pub_scount)
 
 
     # Draws a timeline for the publications of "Wolfgang G. Stock".
-    """
+
     # Stock Publications/Year
     publications = open_from_pickle("pub_stock")
     pub_years = []
@@ -177,10 +177,10 @@ if __name__ == "__main__":
         pub_syears.append(datetime.strptime(str(element[0]) , '%Y'))
         pub_scount.append(element[1])
     draw_timeline(pub_syears, pub_scount, "Anzahl der Publikationen", "Anzahl der Publikationen/Jahr von Wolfgang G. Stock")
-    """
+
 
     # Draws a barchart of all of the authors, who co-authored Wolfgang G. Stock and sorts them by the number of co-published papers
-    """
+
     # Stock Co-Authors
     # Save the data for all publications of Stock in a list
     publications = open_from_pickle("pub_stock")["documents"]
@@ -201,18 +201,18 @@ if __name__ == "__main__":
         authors_name.append(element[0])
         authors_value.append(element[1])
     draw_barchart(authors_name, authors_value, "Anzahl gem. publizierter Artikel", "Co-Autoren von Wolfgang G. Stock")
-    """
+
 
     # Draws a piechart given the frequencys a category on mendeley was tagged with "ontology".
 
     # # Ontology
-    # ontology = open_from_pickle("onto_tagged")
-    # categories = []
-    # values = []
-    # for element in ontology:
-    #     categories.append(element)
-    #     values.append(ontology[element])
-    # draw_barchart(categories, values, "Haeufigkeit der Vergabe", "Mit 'ontology' getaggte Kategorien")
+    ontology = open_from_pickle("onto_tagged")
+    categories = []
+    values = []
+    for element in ontology:
+        categories.append(element)
+        values.append(ontology[element])
+    draw_barchart(categories, values, "Haeufigkeit der Vergabe", "Mit 'ontology' getaggte Kategorien")
 
     # Draws a barchart for the ranked top 10 publications of the journal "Nature and Science"
     # Top 10 Nature and Science
